75a4fadf756cdda6815eab9d9e83ac1b63d09e58
Andrew Morrison <andrew@campus-020-125.ucdavis.edu>
2019-10-21 18:34:28 -0700

    resources add

---
+++ resources.py
@@ -0,0 +1,13 @@
+#keys for reddit api
+r_clientid = 'rJmTVJhI8RHW2A'
+r_clientsecret = '1bK_MWyfXBRtpE3kCFT1rNWIR98'
+r_username = 'razzdraz'
+r_password = 'yedx6icaVe'
+r_useragent = 'nba_comments by /u/razzdraz'
+
+#mongoDB keys
+mongo_user = 'ajmorrison'
+mongo_pass = 'yedx6icaVe'
+mongo_clusteruri = 'cluster0-q82px.gcp.mongodb.net/test?retryWrites=true&w=majority'
+
+connection_uri = 'mongodb+srv://ajmorrison:yedx6icaVe@cluster0-q82px.gcp.mongodb.net/test?retryWrites=true&w=majority'
