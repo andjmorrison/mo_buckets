2d38e6b4a237acfb197a1f43fe91971f5b7ae4bd
Andrew Morrison <andrew@Ursula-Iguaran.local>
2019-10-22 20:25:09 -0700

    working selector, need to add default photo conditional

--- static/js/test.js
+++ static/js/test.js
@@ -1,7 +1,15 @@
+let urlPhoto = []
+
 function buildChart (playerId, playerTeamId) {
+
     d3.json(`/shotchart/${playerId}/${playerTeamId}`).then( individual =>{
 
-        console.log(individual)
+        // name
+        playerName = individual[0][4]
+        // console.log(playerName)
+
+        // coords
+        // console.log(individual)
 
         let shotCoordX = []
         let shotCoordY = []
@@ -25,8 +33,19 @@ function buildChart (playerId, playerTeamId) {
 
         })
 
-        console.log(shotCoordX)
-        console.log(shotCoordY)
+        // console.log(shotCoordX)
+        // console.log(shotCoordY)
+
+        urlPhoto = `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/${playerTeamId}/2019/260x190/${playerId}.png`
+        // urlPhoto = 'https://scoutfly.cbsistatic.com/bundles/scoutcss/images/default/default-headshot.png'
+
+        
+
+        console.log(urlTest)
+
+        
+
+        console.log(urlPhoto)
 
         var trace = {
             x: shotCoordX,
@@ -61,16 +80,28 @@ function buildChart (playerId, playerTeamId) {
                 pad: 4
             },
             paper_bgcolor: '#7f7f7f',
-            plot_bgcolor: 'white'
-        };
+            plot_bgcolor: 'white',
+            images: [
+                {
+                  x: 1,
+                  y: .832,
+                  sizex: 0.2,
+                  sizey: 0.2,
+                  source: urlPhoto,
+                  xanchor: 'right',
+                  xref: 'paper',
+                  yanchor: 'bottom',
+                  yref: 'paper'
+                }
+              ],
+        }
 
-        console.log(trace)
+        // console.log(trace)
 
         Plotly.newPlot('shotchart', data, layout);
 
     })
 }
-
 
 function init() {
     // Grab a reference to the dropdown select element
@@ -79,7 +110,7 @@ function init() {
     // Use the list of sample names to populate the select options
     d3.json("/players").then( players => {
 
-        console.log(players)
+        // console.log(players)
 
         let testing = Object.entries(players)
 
@@ -101,8 +132,8 @@ function init() {
             // console.log(playerArray)
             // console.log(typeof playerArray)
 
-            console.log(playerObject)
-            console.log(typeof playerObject)
+            // console.log(playerObject)
+            // console.log(typeof playerObject)
 
             selector
             .append('option')
@@ -110,14 +141,59 @@ function init() {
             .attr('value', [playerObject.id, playerObject.team])
             // .property("value", playerObject)
         })
+    
+
+        // let selector = d3.select("#selectPlayer")
+        let selectId = document.getElementById('selectPlayer')
+
+        // sort function
+        function sortSelectOptions(selector) {
+
+            let tmpAry = new Array()
+            for (let i=0;i<selector.options.length;i++) {
+                tmpAry[i] = new Array()
+                tmpAry[i][0] = selector.options[i].text
+                tmpAry[i][1] = selector.options[i].value
+            }
+            tmpAry.sort();
+            while (selector.options.length > 0) {
+                selector.options[0] = null
+            }
+            for (let i=0;i<tmpAry.length;i++) {
+                let op = new Option(tmpAry[i][0], tmpAry[i][1])
+                selector.options[i] = op
+            }
+
+            // console.log(tmpAry)
+
+            return
+        }
+
+        // run sort function
+        sortSelectOptions(selectId)
+
+        // build initial chaart from first value
+        let firstPlayer = selectId.options[0].value
+        // console.log(selectId.options[0].value)
+
+        function firstBuild(firstPlayer) {
+
+            let data = firstPlayer.split(',')
+        
+            // console.log(data[0], data[1])
+            // console.log(typeof data)
+        
+            // Fetch new data each time a new player is selected
+            console.log(`Plotting stats for player ${data[0]} on team ${data[1]}`)
+            buildChart(data[0],data[1])
+        }
+        firstBuild(firstPlayer)
     })
 }
 
+// let selector = d3.select("#selectPlayer")
+// let selector = document.getElementById('selectPlayer')
 
-//     // first player for initial chaart
-//     const firstPlayer = playerNames[0];
-//     buildCharts(firstPlayer);
-//     })
 
 // use functions based on selected player
 function optionChanged(newPlayer) {
@@ -127,10 +203,7 @@ function optionChanged(newPlayer) {
     console.log(data[0], data[1])
     console.log(typeof data)
 
-    // console.log(JSON.stringify(newPlayer))
-
-
-  // Fetch new data each time a new player is selected
+    // Fetch new data each time a new player is selected
     console.log(`Plotting stats for player ${data[0]} on team ${data[1]}`)
     buildChart(data[0],data[1])
 }
